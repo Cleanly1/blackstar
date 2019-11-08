4d96967d7157f1c9f6f9dedf004b2bb2a8aa938d
Cleanly <oskar.turesson99@gmail.com>
2019-11-08 23:14:07 +0100

    added navbar styling for mobile

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+const showMobileMenu = function(){
+  window.document.getElementsByClassName('hamburgerContent')[0].style.display = 'block';
+}
+
+
+
+
+
+
+
+
+
+
+window.document.getElementsByClassName('hamburgerMenu')[0].addEventListener("click", showMobileMenu);
