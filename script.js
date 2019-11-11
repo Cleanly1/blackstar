2396ca47171cddc945416c4eda43c811e6f5f69c
Cleanly <oskar.turesson99@gmail.com>
2019-11-11 19:31:54 +0100

    change from inline eventlistener to javascript eventlistener

--- script.js
+++ script.js
@@ -4,22 +4,23 @@
 
 var ifToggled = 'no';
 
-const showMobileMenu = function(x){
+const showMobileMenu = function(){
   let hamburger = document.getElementsByClassName('hamburgerContent')[0];
   let mobileNavbar = document.getElementsByClassName('navbarMobileBox')[0];
-  console.log(ifToggled)
+  let toggle = window.document.getElementsByClassName('hamburgerMenu')[0];
+  
   if (ifToggled === 'no') {
-    x.classList.toggle("change");
+    toggle.classList.toggle("change");
     document.getElementsByClassName('hamburgerContent')[0].style.display = 'block';
     document.getElementsByClassName('bar1')[0].style.background = 'white';
     document.getElementsByClassName('bar3')[0].style.background = 'white';
-    mobileNavbar.style.background = 'rgba(0, 0, 0, 0.9)';
+    mobileNavbar.style.background = 'rgba(0, 0, 0, 0.8)';
     window.document.body.style.overflow = 'hidden';
     ifToggled = 'yes';
   }
   else if (ifToggled === 'yes') {
     ifToggled = 'no';
-    x.classList.toggle("change");
+    toggle.classList.toggle("change");
     document.getElementsByClassName('hamburgerContent')[0].style.display = 'none';
     document.getElementsByClassName('bar1')[0].style.background = 'black';
     document.getElementsByClassName('bar3')[0].style.background = 'black';
@@ -30,6 +31,7 @@ const showMobileMenu = function(x){
   
 }
 
+window.document.getElementsByClassName('hamburgerMenu')[0].addEventListener('click', showMobileMenu);
 
 
 
