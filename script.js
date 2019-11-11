4a796ba4b7370697fe9aec37d8f17fa47a85ed4b
Cleanly <oskar.turesson99@gmail.com>
2019-11-11 14:56:23 +0100

    fixed hamburger to x function

--- script.js
+++ script.js
@@ -2,23 +2,39 @@
 
 
 
+var ifToggled = 'no';
 
-const showMobileMenu = function(){
-  window.document.getElementsByClassName('hamburgerContent')[0].style.display = 'block';
-  window.document.body.style.overflow = 'hidden';
+const showMobileMenu = function(x){
+  let hamburger = document.getElementsByClassName('hamburgerContent')[0];
+  let mobileNavbar = document.getElementsByClassName('navbarMobileBox')[0];
+  console.log(ifToggled)
+  if (ifToggled === 'no') {
+    x.classList.toggle("change");
+    document.getElementsByClassName('hamburgerContent')[0].style.display = 'block';
+    document.getElementsByClassName('bar1')[0].style.background = 'white';
+    document.getElementsByClassName('bar3')[0].style.background = 'white';
+    mobileNavbar.style.background = 'rgba(0, 0, 0, 0.9)';
+    window.document.body.style.overflow = 'hidden';
+    ifToggled = 'yes';
+  }
+  else if (ifToggled === 'yes') {
+    ifToggled = 'no';
+    x.classList.toggle("change");
+    document.getElementsByClassName('hamburgerContent')[0].style.display = 'none';
+    document.getElementsByClassName('bar1')[0].style.background = 'black';
+    document.getElementsByClassName('bar3')[0].style.background = 'black';
+    mobileNavbar.style.background = 'white';
+    window.document.body.style.overflow = 'scroll';
+    
+  }
+  
 }
-const hideMobileMenu = function(){
-  window.document.getElementsByClassName('hamburgerContent')[0].style.display = 'none';
-  window.document.body.style.overflow = 'scroll';
-}
 
 
 
 
 
 
-
-
-
-window.document.getElementsByClassName('hamburgerMenu')[0].addEventListener("click", showMobileMenu);
-window.document.getElementsByClassName('backButton')[0].addEventListener("click", hideMobileMenu);
+ 
+// window.document.getElementsByClassName('hamburgerMenu')[0].addEventListener("click", showMobileMenu);
+// window.document.getElementsByClassName('backButton')[0].addEventListener("click", hideMobileMenu);
