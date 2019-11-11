6c3a6b87955bed1a6c463aa5433918a513136963
Cleanly <oskar.turesson99@gmail.com>
2019-11-11 20:24:34 +0100

    cleaned up code and added description

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-
-
-
-
-var ifToggled = 'no';
-
+// when page is loaded the navbar is hidden so it´s not toggled
+var ifToggled = 'no'; 
+// shows or hides the navigation on mobile devices
 const showMobileMenu = function(){
+  
   let hamburger = document.getElementsByClassName('hamburgerContent')[0];
   let mobileNavbar = document.getElementsByClassName('navbarMobileBox')[0];
   let toggle = window.document.getElementsByClassName('hamburgerMenu')[0];
-  
+  // toggles the navbar on if it´s not already toggled on
   if (ifToggled === 'no') {
     toggle.classList.toggle("change");
     document.getElementsByClassName('hamburgerContent')[0].style.height = '100vh';
     document.getElementsByClassName('bar1')[0].style.background = 'white';
     document.getElementsByClassName('bar3')[0].style.background = 'white';
+    
     for (var i = 0; i < document.getElementsByClassName('menuitem').length; i++) {
       document.getElementsByClassName('menuitem')[i].style.opacity = '1';
     }
+    
     mobileNavbar.style.background = 'rgba(0, 0, 0, 0.8)';
     window.document.body.style.overflow = 'hidden';
+    // to keep
     ifToggled = 'yes';
   }
+  // toggles the navbar off if it´s toggled on
   else if (ifToggled === 'yes') {
     ifToggled = 'no';
     toggle.classList.toggle("change");
     document.getElementsByClassName('hamburgerContent')[0].style.height = '0';
     document.getElementsByClassName('bar1')[0].style.background = 'black';
     document.getElementsByClassName('bar3')[0].style.background = 'black';
+    
     for (var i = 0; i < document.getElementsByClassName('menuitem').length; i++) {
       document.getElementsByClassName('menuitem')[i].style.opacity = '0';
     }
+    
     mobileNavbar.style.background = 'none';
     window.document.body.style.overflow = 'scroll';
+    
     if (scrollY > 100) {
       window.document.getElementsByClassName('navbarMobileBox')[0].style.background = 'white'
     }
@@ -50,12 +55,4 @@ const displayScroll = function() {
 
 
 window.onscroll = displayScroll;
-window.document.getElementsByClassName('hamburgerMenu')[0].addEventListener('click', showMobileMenu);
-
-
-
-
-
- 
-// window.document.getElementsByClassName('hamburgerMenu')[0].addEventListener("click", showMobileMenu);
-// window.document.getElementsByClassName('backButton')[0].addEventListener("click", hideMobileMenu);
+window.document.getElementsByClassName('hamburgerMenu')[0].addEventListener('click', showMobileMenu); 
