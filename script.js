8df785a95b09df17b27695b14dd726ee07b0ae77
Cleanly <oskar.turesson99@gmail.com>
2019-11-12 12:07:42 +0100

    cleaned up more code

--- script.js
+++ script.js
@@ -1,39 +1,27 @@
-// when page is loaded the navbar is hidden so it´s not toggled
-var ifToggled = 'no'; 
-// shows or hides the navigation on mobile devices
+// shows or hides the navigation on mobile devices 
 const showMobileMenu = function(){
   
   let hamburger = document.getElementsByClassName('hamburgerContent')[0];
   let mobileNavbar = document.getElementsByClassName('navbarMobileBox')[0];
   let toggle = window.document.getElementsByClassName('hamburgerMenu')[0];
+  let menuItem = document.getElementsByClassName('menuItem');
   
-  toggle.classList.toggle("change");
-  hamburger.classlist.toggle("");
+  let isToggled = toggle.classList.toggle("change")
+  document.body.classList.toggle("scrollHidden")
+  hamburger.classList.toggle("hamburgerContentShow");
   
-  // toggles the navbar on if it´s not already toggled on
-  if (ifToggled === 'no') { 
-    hamburger.style.height = '100vh';
-
-    for (var i = 0; i < document.getElementsByClassName('menuitem').length; i++) {
-      document.getElementsByClassName('menuitem')[i].style.opacity = '1';
-    }
+  for (var i = 0; i < menuItem.length; i++) {
+    menuItem[i].classList.toggle("menuItemShow");
+  }
+  
+  // changes color of navbarMobileBox
+  if (isToggled === true) { 
     
     mobileNavbar.style.background = 'rgba(0, 0, 0, 0.8)';
-    window.document.body.style.overflow = 'hidden';
-    // to keep track if the navbar is toggled or not
-    ifToggled = 'yes';
+    
   }
-  // toggles the navbar off if it´s toggled on
-  else if (ifToggled === 'yes') {
-    ifToggled = 'no'; 
-    hamburger.style.height = '0'; 
-    
-    for (var i = 0; i < document.getElementsByClassName('menuitem').length; i++) {
-      document.getElementsByClassName('menuitem')[i].style.opacity = '0';
-    }
-    
-    
-    window.document.body.style.overflow = 'scroll';
+  // changes color of navbarMobileBox
+  else if (isToggled === false) {
     
     if (scrollY > 100) {
       document.querySelector('.navbarMobileBox').style.background = 'white'
